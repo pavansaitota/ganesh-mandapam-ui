6d7505dfa793127cbeaa1c2ee8ca9dcf4f08d2f0
pavansaitota <pavansaitota@outlook.com>
2025-11-10 13:04:24 +0530

    fix netlify base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",   // 👈 IMPORTANT FOR NETLIFY ROUTING
   plugins: [
     react({
       babel: {
