c5c849525837abab5cf8784d5f48b6d002dddf88
pavansaitota <pavansaitota@outlook.com>
2025-11-10 13:32:23 +0530

    fix allowedHosts for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",   // 👈 IMPORTANT FOR NETLIFY ROUTING
+  base: '/',                     // keep this
+  server: {
+    allowedHosts: ['.netlify.app'],  // allow all netlify preview hosts
+  },
   plugins: [
     react({
       babel: {
